21a310569e55922a39440d62a577a0ded4c47bca
Wim <wim@42.be>
2015-10-29 11:52:08 +0100

    Add some extra options

--- mattertee/main.go
+++ mattertee/main.go
@@ -2,23 +2,38 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"github.com/42wim/matterbridge/matterhook"
 	"io"
 	"os"
 )
 
+var (
+	flagUserName, flagChannel, flagIconURL string
+	flagPlainText                          bool
+)
+
+func init() {
+	flag.StringVar(&flagUserName, "u", "mattertee", "This username is used for posting.")
+	flag.StringVar(&flagChannel, "c", "", " Post input values to specified channel or user.")
+	flag.StringVar(&flagIconURL, "i", "", "This url is used as icon for posting.")
+	flag.BoolVar(&flagPlainText, "p", false, "Don't surround the post with triple backticks.")
+	flag.Parse()
+}
+
 func main() {
-	var title string
 	url := os.Getenv("MM_HOOK")
 	m := matterhook.New(url, matterhook.Config{DisableServer: true})
 	msg := matterhook.OMessage{}
-	msg.UserName = "mattertee"
+	msg.UserName = flagUserName
+	msg.Channel = flagChannel
+	msg.IconURL = flagIconURL
 	buf := new(bytes.Buffer)
 	io.Copy(buf, os.Stdin)
-	if len(os.Args) > 1 {
-		title = os.Args[1]
+	if flagPlainText {
+		msg.Text = buf.String()
+	} else {
+		msg.Text = "```" + buf.String() + "```"
 	}
-	msg.Text = title + "\n"
-	msg.Text = msg.Text + "```" + buf.String() + "```"
 	m.Send(msg)
 }
