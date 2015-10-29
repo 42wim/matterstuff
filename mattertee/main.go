023a90c392e3586bf7402525ddd61607378ea501
Wim <wim@42.be>
2015-10-29 12:26:55 +0100

    Add no-buffer support

--- mattertee/main.go
+++ mattertee/main.go
@@ -6,11 +6,12 @@ import (
 	"github.com/42wim/matterbridge/matterhook"
 	"io"
 	"os"
+	"strings"
 )
 
 var (
 	flagUserName, flagChannel, flagIconURL string
-	flagPlainText                          bool
+	flagPlainText, flagNoBuffer            bool
 )
 
 func init() {
@@ -18,7 +19,12 @@ func init() {
 	flag.StringVar(&flagChannel, "c", "", " Post input values to specified channel or user.")
 	flag.StringVar(&flagIconURL, "i", "", "This url is used as icon for posting.")
 	flag.BoolVar(&flagPlainText, "p", false, "Don't surround the post with triple backticks.")
+	flag.BoolVar(&flagNoBuffer, "n", false, "Post input values without buffering.")
 	flag.Parse()
+}
+
+func md(text string) string {
+	return "```" + text + "```"
 }
 
 func main() {
@@ -30,10 +36,20 @@ func main() {
 	msg.IconURL = flagIconURL
 	buf := new(bytes.Buffer)
 	io.Copy(buf, os.Stdin)
+	msg.Text = md(buf.String())
 	if flagPlainText {
 		msg.Text = buf.String()
-	} else {
-		msg.Text = "```" + buf.String() + "```"
 	}
-	m.Send(msg)
+	if flagNoBuffer {
+		texts := strings.Split(buf.String(), "\n")
+		for _, text := range texts {
+			msg.Text = md(text)
+			if flagPlainText {
+				msg.Text = text
+			}
+			m.Send(msg)
+		}
+	} else {
+		m.Send(msg)
+	}
 }
